0941089174b0e9ab83912ddead423720d568f9ab
ghink <glenn.v.hinks@aexp.com>
2018-04-14 14:15:26 -0400

    add request-promise

--- src/fetchVersions/index.js
+++ src/fetchVersions/index.js
@@ -1,12 +1,10 @@
-import fetch from 'isomorphic-fetch'
+import request from 'request-promise'
 import npa from 'npm-package-arg'
 
 const action = async (packageName, registry) => {
   const escapedName = npa(packageName).escapedName
   const url = `${registry}${escapedName}`
-  const options = { method: 'get', timeout: 20000 }
-  const data = await fetch(url, options)
-  const response = await data.json()
+  const response = await request(url)
   return response
 }
 
